cdf00836d12761924159a550df82f96733f2a23e
ProgrammerShahJalal <you@example.com>
2024-09-21 12:53:02 +0600

    first commit

---
+++ src/components/UI/NavbarDropdown.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/dropdown";
+
+import { useRouter } from "next/navigation";
+import { Avatar } from "@nextui-org/avatar";
+import { logout } from "@/src/services/AuthService";
+import { useUser } from "@/src/context/user.provider";
+
+export default function NavbarDropdown() {
+  const router = useRouter();
+  const { user, setIsLoading: userLoading } = useUser();
+
+  const handleLogout = () => {
+    logout();
+    userLoading(true);
+  };
+
+  const handleNavigation = (pathname: string) => {
+    router.push(pathname);
+  };
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Avatar className="cursor-pointer" src={user?.profilePhoto} />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem onClick={() => handleNavigation("/profile")}>
+          Profile
+        </DropdownItem>
+        <DropdownItem onClick={() => handleNavigation("/profile/settings")}>
+          Settings
+        </DropdownItem>
+        <DropdownItem onClick={() => handleNavigation("/profile/create-post")}>
+          Create Post
+        </DropdownItem>
+        <DropdownItem
+          onClick={() => handleLogout()}
+          key="delete"
+          className="text-danger"
+          color="danger"
+        >
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
